17457cfb217c6558bbdc63322ddfb2e843402a68
Your Name <shihab.himel111@gmail.com>
2025-04-15 20:54:09 +0600

    updated Health Profile with dto and mapper

---
+++ src/main/java/com/Smart/Health/Care/Management/System/DTO/HealthProfileCreateDto.java
@@ -0,0 +1,57 @@
+package com.Smart.Health.Care.Management.System.DTO;
+
+import java.util.List;
+public class HealthProfileCreateDto {
+
+    private Long patientId;
+    private double height;
+    private double weight;
+    private String bloodGroup;
+    private List<String> allergies;
+    private String medicalHistory;
+    private String conditions;
+
+    public Long getPatientId() {
+        return patientId;
+    }
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+    public List<String> getAllergies() {
+        return allergies;
+    }
+    public void setAllergies(List<String> allergies) {
+        this.allergies = allergies;
+    }
+    public String getMedicalHistory() {
+        return medicalHistory;
+    }
+    public void setMedicalHistory(String medicalHistory) {
+        this.medicalHistory = medicalHistory;
+    }
+    public String getConditions() {
+        return conditions;
+    }
+    public void setConditions(String conditions) {
+        this.conditions = conditions;
+    }
+
+}
